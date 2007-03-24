bf84f142049f63dce065b2d50651e8ee9d65a792
ksi84 <ksi84>
2007-03-24 14:55:40 +0000

    *** empty log message ***

---
+++ sn/SONStock/DataLoader.cs
@@ -0,0 +1,210 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace SONStock
+{
+    class DataLoader
+    {
+        private SortedDictionary<DateTime, double> data = new SortedDictionary<DateTime, double>();
+
+        public SortedDictionary<DateTime, double> Data
+        {
+            get { return data; }
+        }
+
+        public void ClearData()
+        {
+            data.Clear();
+        }
+
+        public int Count
+        {
+            get 
+            {
+                if (data != null)
+                    return data.Count;
+                else
+                    return 0;
+            }
+        }
+
+        private void AddNewDataFromFile(String filename, bool clear)
+        {
+            if (clear)
+                data.Clear();
+            using (TextReader rd = new StreamReader(filename))
+            {
+                String str = null;
+                string flo = "[+|-]?[0-9]*[,[0-9]*]?";
+                string date = "[0-9]*-[0-9][0-9]-[0-9][0-9]";
+                string wzorzec = "\"(?<data>" + date + "?)\",\"" +
+                    "(?<fl1>" + flo + "?)\",\"" +
+                    "(?<fl2>" + flo + "?)\",\"" +
+                    "(?<fl3>" + flo + "?)\",\"" +
+                    "(?<fl4>" + flo + "?)\",\"" +
+                    "(?<fl5>" + flo + "?)\"";
+                Regex wyr = new Regex(wzorzec, RegexOptions.IgnoreCase);
+                while ((str = rd.ReadLine()) != null)
+                {
+                    try
+                    {
+                        str = str.Trim();
+                        Match elem = wyr.Match(str);
+                        string[] tablica = wyr.GetGroupNames();
+                        if (elem.Success)
+                        {
+                            string d = elem.Groups["data"].Value;
+                            double zamk = (double)Double.Parse(elem.Groups["fl4"].Value);
+                            DateTime dt = new DateTime(Int32.Parse(d.Substring(0, 4)),
+                                                        Int32.Parse(d.Substring(5, 2)),
+                                                        Int32.Parse(d.Substring(8, 2)));
+                            data.Add(dt, zamk);
+                        }
+                        else
+                        {
+                            //MessageBox.Show("pora¿ka lub koniec danych:)");
+                        }
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+
+        public ElmansNetwork LoadNetwork()
+        {
+            ElmansNetwork elmanNet = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "pliki csv (*.xml)|*.xml";
+            ofd.Title = "Wybierz plik z sieci¹";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Opens file "data.xml" and deserializes the object from it.
+                    Stream stream = File.Open(ofd.FileName, FileMode.Open);
+                    SoapFormatter formatter = new SoapFormatter();
+
+                    //formatter = new BinaryFormatter();
+
+                    elmanNet = (ElmansNetwork)formatter.Deserialize(stream);
+                    stream.Close();
+
+                    Properties.Settings.Default.estimationTime = elmanNet.NumberOfExitNeurons;
+                    Properties.Settings.Default.hiddenLayerSize = elmanNet.NumberOfHiddenNeurons;
+                    Properties.Settings.Default.entryLayerSize = elmanNet.NumberOfEntryNeurons;
+                    Properties.Settings.Default.Save();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Deserializacja zakoñczy³a siê niepowodzeniem");
+                }
+            }
+            return elmanNet;
+        }
+
+        public void SaveNetwork(ElmansNetwork elmanNet)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Pliki xml (*.xml)|*.xml";
+            sfd.Title = "Wybierz plik z sieci¹";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //Opens a file and serializes the object into it in binary format.
+                Stream stream = File.Open(sfd.FileName, FileMode.Create);
+                SoapFormatter formatter = new SoapFormatter();
+
+                //BinaryFormatter formatter = new BinaryFormatter();
+
+                formatter.Serialize(stream, elmanNet);
+                stream.Close();
+            }
+        }
+
+        public void LoadDataFromFile(bool clear)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Pliki csv (*.csv)|*.csv";
+            ofd.Title = "Wybierz plik zawieraj¹cy dane";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.AddNewDataFromFile(ofd.FileName, clear);
+                //this.addDataToExistingToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        public void LoadDataFromFiles(bool clear)
+        {
+            OpenDirectoryDialog odd = new OpenDirectoryDialog();
+            string directory;
+            if ((directory = odd.GetFolder()) != null && directory != "")
+            {
+                string[] files = Directory.GetFiles(directory, "*.csv");
+                if (files.Length > 0)
+                {
+                    if (clear)
+                        this.data.Clear();
+                    for (int i = 0; i < files.Length; ++i)
+                    {
+                        this.AddNewDataFromFile(files[i], false);
+                    }
+                    //this.addDataToExistingToolStripMenuItem.Enabled = true;
+                }
+                else
+                    MessageBox.Show("Niestety nie znaleziono ¿adnych plików *.csv");
+            }
+        }
+
+        public ElmansNetwork Learn(ElmansNetwork elmanNet)
+        {
+            if (data == null || data.Count == 0)
+            {
+                MessageBox.Show("Wczytaj zestaw ucz¹cy");
+                return elmanNet;
+            }
+
+            IEnumerator enumerator = data.Keys.GetEnumerator();
+            double[] values = new double[data.Count];
+            int i = 0;
+
+            int entryLayerSize = Properties.Settings.Default.entryLayerSize;
+            int exitLayerSize = Properties.Settings.Default.estimationTime;
+            int hiddenLayerSize = Properties.Settings.Default.hiddenLayerSize;
+
+            while (enumerator.MoveNext())
+            {
+                DateTime dt = (DateTime)enumerator.Current;
+                values[i++] = data[dt];
+            }
+
+            if (elmanNet == null ||
+                elmanNet.NumberOfEntryNeurons != entryLayerSize ||
+                elmanNet.NumberOfExitNeurons != exitLayerSize)
+                elmanNet = new ElmansNetwork(entryLayerSize, hiddenLayerSize, exitLayerSize);
+
+            double[] val = new double[entryLayerSize];
+            double[] correct = new double[exitLayerSize];
+
+            for (i = 0; i < values.Length - entryLayerSize - exitLayerSize; ++i)
+            {
+                for (int j = 0; j < entryLayerSize; ++j)
+                {
+                    val[j] = values[i + j];
+                }
+                for (int j = 0; j < exitLayerSize; ++j)
+                {
+                    correct[j] = values[i + j + entryLayerSize];
+                }
+                elmanNet.Learn(val, correct);
+            }
+            return elmanNet;
+        }
+    }
+}
--- sn/SONStock/Form1.cs
+++ sn/SONStock/Form1.cs
@@ -1,23 +1,19 @@
 using System;
-using System.Collections.Generic;
-using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Soap;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace SONStock
 {
     public partial class Form1 : Form
     {
-        private SortedDictionary<DateTime, double> data = new SortedDictionary<DateTime, double>();
         private ElmansNetwork elmanNet;
         private MatrixPreviewForm matrixPreviewForm;
+        private DataLoader data = new DataLoader();
 
         public Form1()
         {
@@ -40,99 +36,19 @@ namespace SONStock
             this.exitValuesMatrixPreview.BuildControl(exit);
         }
 
-        
-
-        private void AddNewDataFromFile(String filename, bool clear)
-        {
-            if (clear)
-                data.Clear();
-            using (TextReader rd = new StreamReader(filename))
-            {
-                String str = null;
-                string flo = "[+|-]?[0-9]*[,[0-9]*]?";
-                string date = "[0-9]*-[0-9][0-9]-[0-9][0-9]";
-                string wzorzec = "\"(?<data>" + date + "?)\",\"" +
-                    "(?<fl1>" + flo + "?)\",\"" +
-                    "(?<fl2>" + flo + "?)\",\"" +
-                    "(?<fl3>" + flo + "?)\",\"" +
-                    "(?<fl4>" + flo + "?)\",\"" +
-                    "(?<fl5>" + flo + "?)\"";
-                Regex wyr = new Regex(wzorzec, RegexOptions.IgnoreCase);
-                while ((str = rd.ReadLine()) != null)
-                {
-                    try
-                    {
-                        str = str.Trim();
-                        Match elem = wyr.Match(str);
-                        string[] tablica = wyr.GetGroupNames();
-                        if (elem.Success)
-                        {
-                            string d = elem.Groups["data"].Value;
-                            double zamk = (double)Double.Parse(elem.Groups["fl4"].Value);
-                            DateTime dt = new DateTime(Int32.Parse(d.Substring(0, 4)),
-                                                        Int32.Parse(d.Substring(5, 2)),
-                                                        Int32.Parse(d.Substring(8, 2)));
-                            data.Add(dt, zamk);
-                        }
-                        else
-                        {
-                            //MessageBox.Show("pora¿ka lub koniec danych:)");
-                        }
-                    }
-                    catch (Exception) { }
-                }
-            }
-        }
-
         #region NetworkToolStripMenu
         private void loadNetworkToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "pliki csv (*.xml)|*.xml";
-            ofd.Title = "Wybierz plik z sieci¹";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    //Opens file "data.xml" and deserializes the object from it.
-                    Stream stream = File.Open(ofd.FileName, FileMode.Open);
-                    SoapFormatter formatter = new SoapFormatter();
-
-                    //formatter = new BinaryFormatter();
-
-                    this.elmanNet = (ElmansNetwork)formatter.Deserialize(stream);
-                    stream.Close();
-
-                    Properties.Settings.Default.estimationTime = elmanNet.NumberOfExitNeurons;
-                    Properties.Settings.Default.hiddenLayerSize = elmanNet.NumberOfHiddenNeurons;
-                    Properties.Settings.Default.entryLayerSize = elmanNet.NumberOfEntryNeurons;
-                    Properties.Settings.Default.Save();
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Deserializacja zakoñczy³a siê niepowodzeniem");
-                }
-            }
+            //TODO
+            this.elmanNet = this.data.LoadNetwork();
         }
 
         private void saveNetworkToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (this.elmanNet != null)
             {
-                SaveFileDialog sfd = new SaveFileDialog();
-                sfd.Filter = "Pliki xml (*.xml)|*.xml";
-                sfd.Title = "Wybierz plik z sieci¹";
-                if (sfd.ShowDialog() == DialogResult.OK)
-                {
-                    //Opens a file and serializes the object into it in binary format.
-                    Stream stream = File.Open(sfd.FileName, FileMode.Create);
-                    SoapFormatter formatter = new SoapFormatter();
-
-                    //BinaryFormatter formatter = new BinaryFormatter();
-
-                    formatter.Serialize(stream, elmanNet);
-                    stream.Close();
-                }
+                //TODO
+                this.data.SaveNetwork(this.elmanNet);
             }
             else
             {
@@ -142,47 +58,8 @@ namespace SONStock
 
         private void learnNetworkToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (data == null || data.Count==0)
-            {
-                MessageBox.Show("Wczytaj zestaw ucz¹cy");
-                return;
-            }
-
-            IEnumerator enumerator = data.Keys.GetEnumerator();
-            double[] values = new double[data.Count];
-            int i = 0;
-
-            int entryLayerSize = Properties.Settings.Default.entryLayerSize;
-            int exitLayerSize = Properties.Settings.Default.estimationTime;
-            int hiddenLayerSize = Properties.Settings.Default.hiddenLayerSize;
-
-            while (enumerator.MoveNext())
-            {
-                DateTime dt = (DateTime)enumerator.Current;
-                values[i++] = data[dt];
-            }
-
-            if (elmanNet == null || 
-                elmanNet.NumberOfEntryNeurons != entryLayerSize || 
-                elmanNet.NumberOfExitNeurons != exitLayerSize)
-                elmanNet = new ElmansNetwork(entryLayerSize, hiddenLayerSize, exitLayerSize);
-            
-            double[] val = new double[entryLayerSize];
-            double[] correct = new double[exitLayerSize];
-            
-            for (i = 0; i < values.Length - entryLayerSize - exitLayerSize; ++i)
-            {
-                for (int j = 0; j < entryLayerSize; ++j)
-                {
-                    val[j] = values[i + j];
-                }
-                for (int j = 0; j < exitLayerSize; ++j)
-                {
-                    correct[j] = values[i + j + entryLayerSize];
-                }
-                elmanNet.Learn(val, correct);
-            }
-
+//TODO
+            this.elmanNet = this.data.Learn(this.elmanNet);
             if (data != null && elmanNet != null && data.Count >= elmanNet.NumberOfEntryNeurons)
                 this.performEstimationToolStripMenuItem.Enabled = true;
         }
@@ -206,53 +83,37 @@ namespace SONStock
         #region EstimateToolStripMenu
         private void loadDataFromFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Pliki csv (*.csv)|*.csv";
-            ofd.Title = "Wybierz plik zawieraj¹cy dane";
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                this.AddNewDataFromFile(ofd.FileName, this.addDataToExistingToolStripMenuItem.Enabled);
+            this.data.LoadDataFromFile(this.addDataToExistingToolStripMenuItem.Enabled);
+//TODO
+            if (this.data.Count > 0)
                 this.addDataToExistingToolStripMenuItem.Enabled = true;
-                if(elmanNet != null && data != null && data.Count>=elmanNet.NumberOfEntryNeurons)
-                    this.performEstimationToolStripMenuItem.Enabled = true;
-            }
+            if (elmanNet != null && data != null && data.Count >= elmanNet.NumberOfEntryNeurons)
+                this.performEstimationToolStripMenuItem.Enabled = true;
         }
 
         private void loadDataFromManyFilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenDirectoryDialog odd = new OpenDirectoryDialog();
-            string directory;
-            if ((directory = odd.GetFolder()) != null && directory != "")
-            {
-                string[] files = Directory.GetFiles(directory, "*.csv");
-                if (files.Length > 0)
-                {
-                    if (!this.addDataToExistingToolStripMenuItem.Enabled)
-                        this.data.Clear();
-                    for (int i = 0; i < files.Length; ++i)
-                    {
-                        this.AddNewDataFromFile(files[i], false);
-                    }
-                    this.addDataToExistingToolStripMenuItem.Enabled = true;
-                    if (elmanNet!=null && data.Count >= elmanNet.NumberOfEntryNeurons)
-                        this.performEstimationToolStripMenuItem.Enabled = true;
-                }
-                else
-                    MessageBox.Show("Niestety nie znaleziono ¿adnych plików *.csv");
-            }
+            this.data.LoadDataFromFiles(this.addDataToExistingToolStripMenuItem.Enabled);
+            if(this.data.Count>0)
+                this.addDataToExistingToolStripMenuItem.Enabled = true;
+            if (elmanNet != null && data.Count >= elmanNet.NumberOfEntryNeurons)
+                this.performEstimationToolStripMenuItem.Enabled = true;
         }
 
         private void performEstimationToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (elmanNet != null && data != null && data.Count >= elmanNet.NumberOfEntryNeurons)
+            SortedDictionary<DateTime, double> d=null;
+            if (this.data != null)
+                d = this.data.Data;
+            if (elmanNet != null && d != null && d.Count >= elmanNet.NumberOfEntryNeurons)
             {
-                double[] values = new double[data.Count];
+                double[] values = new double[d.Count];
                 int counter = 0;
-                IEnumerator enumerator = data.Keys.GetEnumerator();
+                IEnumerator enumerator = d.Keys.GetEnumerator();
                 while (enumerator.MoveNext())
                 {
                     DateTime dt = (DateTime)enumerator.Current;
-                    values[counter++] = data[dt];
+                    values[counter++] = d[dt];
                 }
                 
                 double[] entryValues = new double[elmanNet.NumberOfEntryNeurons];
@@ -267,7 +128,7 @@ namespace SONStock
 
         private void clearDataToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            data.Clear();
+            data.ClearData();
             this.performEstimationToolStripMenuItem.Enabled = false;
         }
         #endregion
