b257615458418514386bf3fcd1be90c139e14b60
mnowinski <mnowinski>
2007-03-24 08:11:53 +0000

    *** empty log message ***

---
+++ taio/TAiO Skeleton/Algorithms/Algorithm1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taio.Algorithms
+{
+    class Algorithm1
+    {
+        private List<Rectangle> rectangles;
+
+        public Algorithm1(List<Rectangle> rectangles)
+        {
+            this.rectangles = rectangles;
+        }
+
+        public Rectangle ComputeMaximumRectangle()
+        {
+
+        }
+    }
+}
