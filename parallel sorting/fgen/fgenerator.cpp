2e5c470a23068018f22865a6ce13456e363f8fd9
mroczek <mroczek>
2007-05-31 21:03:17 +0000

    *** empty log message ***

--- parallel sorting/fgen/fgenerator.cpp
+++ parallel sorting/fgen/fgenerator.cpp
@@ -6,7 +6,6 @@ using namespace tools;
 
 string outputFile;
 int numbers;
-int minNumber = 0;
 int maxNumber = 100;
 int onlyDisplay = 0;
 
@@ -16,7 +15,9 @@ void showUsage()
 	cout<<"fgen outputFile numberCount [numberMax (default=100)]"<<endl;
 }
 
-
+/*
+ * Checks main program input parameters.
+ */ 
 bool checkInput(int argc, char* argv[])
 {	
 	if(argc < 2)
@@ -38,47 +39,63 @@ bool checkInput(int argc, char* argv[])
 	return true;
 }
 
+/*
+ * Displays specified by filepath file's content.
+ */ 
+int displayFileContent(string file)
+{
+	int fd = MyIO::my_open(file.c_str(), O_RDONLY);
+	if(fd == -1)
+		return 1;
+	
+	int n;
+	MyIO::my_read(fd, &n, sizeof(int), 0, SEEK_CUR);
+	int *buffer = new int[n];
+	MyIO::my_read(fd, buffer, sizeof(int) * n, 0, SEEK_CUR);
+	
+	for(int i = 0; i < n; i++)
+		cout<<buffer[i]<<" ";
+	cout<<endl;
+	
+	MyIO::my_close(fd);
+	return 0;
+}
+
+/*
+ * Generates random numbers not greater than maxNumber integers.
+ */ 
+int generateNumbers(int numbers, int maxNumber, string file)
+{
+	int fd, random;
+	srand((unsigned)time(NULL));
+	
+	fd = MyIO::my_open(file.c_str(), O_CREAT);
+	MyIO::my_close(fd);
+	fd = MyIO::my_open(file.c_str(), O_WRONLY);
+	
+	if(fd == -1)
+		return 1;	
+
+	MyIO::my_write(fd, &numbers, sizeof(int), 0, SEEK_CUR);
+	for(int i=0; i<numbers; i++)
+	{
+		random=rand()%(maxNumber+1);
+		MyIO::my_write(fd, &random, sizeof(int), 0, SEEK_CUR);
+	}
+	
+	MyIO::my_close(fd);
+	cout<<"File "<< file << " with "<<numbers<<" integers (max = "<<maxNumber<<") to sort generated."<< endl;
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
-	int random;
-	int fd;
 	if(checkInput(argc, argv))
 	{
 		if(onlyDisplay)
-		{
-			fd = MyIO::my_open(outputFile.c_str(), O_RDONLY);
-			if(fd == -1)
-				return 1;
-			int n;
-			MyIO::my_read(fd, &n, sizeof(int), 0, SEEK_CUR);
-			int *buffer = new int[n];
-			MyIO::my_read(fd, buffer, sizeof(int) * n, 0, SEEK_CUR);
-			for(int i = 0; i < n; i++)
-				cout<<buffer[i]<<" ";
-			cout<<endl;
-			MyIO::my_close(fd);
-		}
+			return displayFileContent(outputFile);
 		else
-		{
-			srand((unsigned)time(NULL));
-	
-			fd = MyIO::my_open(outputFile.c_str(), O_CREAT);
-			MyIO::my_close(fd);
-			fd = MyIO::my_open(outputFile.c_str(), O_WRONLY);
-	
-			if(fd == -1)
-				return 1;	
-
-			MyIO::my_write(fd, &numbers, sizeof(int), 0, SEEK_CUR);
-			for(int i=0; i<numbers; i++)
-			{
-				random=rand()%(maxNumber+1);
-				//cout<<random<<endl;
-					MyIO::my_write(fd, &random, sizeof(int), 0, SEEK_CUR);
-			}
-			MyIO::my_close(fd);
-			cout<<"File "<< outputFile << " with "<<numbers<<" integers (max = "<<maxNumber<<") to sort generated."<< endl;
-		}
+			return generateNumbers(numbers, maxNumber, outputFile);
 	}
 	else
 	{
