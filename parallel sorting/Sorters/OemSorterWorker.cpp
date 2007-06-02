9d7a19c700b08f195e93efb1c14772842bcd6eed
mnowinski <mnowinski>
2007-06-02 11:35:36 +0000

    *** empty log message ***

--- parallel sorting/Sorters/OemSorterWorker.cpp
+++ parallel sorting/Sorters/OemSorterWorker.cpp
@@ -27,17 +27,21 @@ OemSorterWorker::OemSorterWorker(string inFile, string outFile, int argc, char**
  */
 int OemSorterWorker::compareSplit(int idProcess, int myId, int* buffer, int bufSize)
 {	
-	
+	MPI_Request request;
 	int* buffer2 = new int[bufSize*2];
 	MPI_Status status; 
 	OemSorter* sorter = new OemSorter();
-	if(Utils::mpi_send(buffer,bufSize,idProcess,WORK_TAG+50))
-		return 1;
+	//cout<<"CompSplit: "<<myId<<"<->"<<idProcess<<endl;
+	MPI_Isend( buffer, bufSize, MPI_INT, idProcess, WORK_TAG+50, MPI_COMM_WORLD, &request );
+	/*if(Utils::mpi_send(buffer,bufSize,idProcess,WORK_TAG+50))
+		return 1;*/
 	if(Utils::mpi_recv(buffer2,bufSize,idProcess,WORK_TAG+50,&status))
 		return 1;
 	for(int i = 0; i<bufSize; ++i)
 		buffer2[i+bufSize] = buffer[i];
+	//cout<<"CompSplit finished: "<<myId<<"<->"<<idProcess<<endl;
 	sorter->sort(buffer2, bufSize * 2);
+	
 	int shift = 0;
 	if(myId > idProcess)
 		shift = bufSize;
